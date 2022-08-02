cb3071bb362a748f66dff5e58cd22b7d031c5cd0
Berlin Andrey <99011598+Andrey-Berlin@users.noreply.github.com>
2022-08-02 14:48:30 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,132 @@
+
+let mas =['1','2','3','4','5','6','7','8','9',
+          '10','11','12','13','14','15','16','17',
+          '18','19','20','21','22','23','24',
+          '25','26','27','28'] //27 ячеек
+
+let section = document.getElementById("section");
+
+let click = section.getElementsByTagName('li');
+
+for (let i = 0; i < click.length; i ++) {
+  
+  
+  click[i].onclick = function(){
+
+
+         let id = this.getAttribute("id");
+
+         if(id == mas[0]) {
+
+         document.location.href = mas[0]+".html";
+
+         }else if (id == mas[1]) {
+
+         document.location.href = mas[1]+".html";
+
+         }else if (id == mas[2]) {
+
+          document.location.href = mas[2]+".html";
+ 
+          }else if (id == mas[3]) {
+
+            document.location.href = mas[3]+".html";
+   
+            }else if (id == mas[4]) {
+
+              document.location.href = mas[4]+".html";
+     
+              }else if (id == mas[5]) {
+
+                document.location.href = mas[6]+".html";
+       
+                }else if (id == mas[7]) {
+
+                  document.location.href = mas[7]+".html";
+         
+                  }else if (id == mas[8]) {
+
+                    document.location.href = mas[8]+".html";
+           
+                    }else if (id == mas[9]) {
+
+                      document.location.href = mas[9]+".html";
+             
+                      }else if (id == mas[10]) {
+
+                        document.location.href = mas[10]+".html";
+               
+                        }else if (id == mas[11]) {
+
+                          document.location.href = mas[1]+".html";
+                 
+                          }else if (id == mas[12]) {
+
+                            document.location.href = mas[12]+".html";
+                   
+                            }else if (id == mas[13]) {
+
+                              document.location.href = mas[13]+".html";
+                     
+                              }else if (id == mas[14]) {
+
+                                document.location.href = mas[14]+".html";
+                       
+                                }else if (id == mas[15]) {
+
+                                  document.location.href = mas[15]+".html";
+                         
+                                  }else if (id == mas[16]) {
+
+                                    document.location.href = mas[16]+".html";
+                           
+                                    }else if (id == mas[17]) {
+
+                                      document.location.href = mas[17]+".html";
+                             
+                                      }else if (id == mas[18]) {
+
+                                        document.location.href = mas[18]+".html";
+                               
+                                        }else if (id == mas[19]) {
+
+                                          document.location.href = mas[19]+".html";
+                                 
+                                          }else if (id == mas[20]) {
+
+                                            document.location.href = mas[20]+".html";
+                                   
+                                            }else if (id == mas[21]) {
+
+                                              document.location.href = mas[21]+".html";
+                                     
+                                              }else if (id == mas[22]) {
+
+                                                document.location.href = mas[22]+".html";
+                                       
+                                                }else if (id == mas[23]) {
+
+                                                  document.location.href = mas[23]+".html";
+                                         
+                                                  }else if (id == mas[24]) {
+
+                                                    document.location.href = mas[24]+".html";
+                                           
+                                                    }else if (id == mas[25]) {
+
+                                                      document.location.href = mas[25]+".html";
+                                             
+                                                      }else if (id == mas[26]) {
+
+                                                        document.location.href = mas[26]+".html";
+                                               
+                                                        }else if (id == mas[27]) {
+
+                                                          document.location.href = mas[27]+".html";
+                                                 
+                                                          }
+
+   }
+  
+
+}
---
+++ script1.js
@@ -0,0 +1,47 @@
+let gimn1 = document.getElementById('1-2');
+
+gimn1.onclick = function() {
+
+  document.location.href = "1-2.html";
+  
+}
+
+let gimn2 = document.getElementById('3-4');
+
+gimn2.onclick = function() {
+    
+  document.location.href = "3-4.html";
+  
+}
+
+let gimn3 = document.getElementById('5-6');
+
+gimn3.onclick = function() {
+    
+  document.location.href = "5-6.html";
+  
+}
+
+let gimn4 = document.getElementById('1-2');
+
+gimn4.onclick = function() {
+    
+  document.location.href = "1-2.html";
+  
+}
+
+let gimn5 = document.getElementById('1-2');
+
+gimn_1_2.onclick = function() {
+    
+  document.location.href = "1-2.html";
+  
+}
+
+let gimn_1_2 = document.getElementById('1-2');
+
+gimn_1_2.onclick = function() {
+    
+  document.location.href = "1-2.html";
+  
+}
